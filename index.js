cac50996f6c0ee467b062c66f88c4a96b8b95b0d
brevre <brendamakena09@gmail.com>
2023-10-13 21:15:45 +0200

    completed

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Code your solution here
-const batteryBatches = [4, 5, 3, 4, 6, 5, 3];
+const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-const totalBatteries = batteryBatches.reduce((total, batch) => total + batch, 0);
+// Use the reduce method to calculate the total number of batteries
+const totalBatteries = batteryBatches.reduce(function(total, batch) {
+  return total + batch;
+}, 0);
 
-console.log(totalBatteries); // This will output the sum of all battery amounts
+console.log(totalBatteries); 
